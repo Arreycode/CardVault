229a8fedfb6b927ee4e0685eb829eab827b3f247
ebai alice <alice.ebai@student.howest.be>
2024-08-15 05:01:50 +0200

    Implemented GetAllCards method to retrieve all card data with related entities and return as a list.

--- CardVault/Services/Services.cs
+++ CardVault/Services/Services.cs
@@ -1,4 +1,8 @@
-﻿using CardVault.Models;
+﻿using CardVault.ModelsDTO;
+using CardVault.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CardVault.Services
 {
@@ -11,5 +15,39 @@ namespace CardVault.Services
             dbContext = new MyDBContext();
         }
     }
+
+
+    public IList<CardModelClass> GetAllCards()
+    {
+        return dbContext.Cards
+            .Include(c => c.CardColors)
+            .ThenInclude(cc => cc.Color)
+            .Include(c => c.CardTypes)
+            .ThenInclude(ct => ct.Type)
+            .Select(p => new CardModelClass
+            {
+                Id = p.Id,
+                Name = p.Name,
+                ManaCost = p.ManaCost,
+                ConvertedManaCost = p.ConvertedManaCost,
+                Type = string.Join(", ", p.CardTypes.Select(ct => ct.Type.Name)),
+                RarityCode = p.RarityCode,
+                SetCode = p.SetCode,
+                Text = p.Text,
+                Flavor = p.Flavor,
+                Power = p.Power,
+                Toughness = p.Toughness,
+                Layout = p.Layout,
+                MultiverseId = p.MultiverseId,
+                OriginalImageUrl = p.OriginalImageUrl ?? "images/placeholder.png", // Handle nulls by providing a default value
+                Image = p.Image,
+                Colors = p.CardColors.Select(cc => cc.Color.Name).ToList(),
+                Types = p.CardTypes.Select(ct => ct.Type.Name).ToList()
+            })
+            .OrderBy(o => o.Name)
+            .Take(150) // Consider initial page size
+            .ToList();
+    }
+
 
 }
