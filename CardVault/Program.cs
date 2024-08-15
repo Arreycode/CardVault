5747824ba44e6aefbe93935820e7c14890dc1d4a
ebai alice <alice.ebai@student.howest.be>
2024-08-15 04:28:49 +0200

    Configure dependency injection for services and DbContext

--- CardVault/Program.cs
+++ CardVault/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddScoped<CardVault.Services.Services>();
 
 var app = builder.Build();
 
---
+++ CardVault/Services/Services.cs
@@ -0,0 +1,6 @@
+﻿namespace CardVault.Services
+{
+    public class Services
+    {
+    }
+}
