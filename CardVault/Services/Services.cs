0a1b2bb191f571c05e4436a65a9537bb85a71b0c
ebai alice <alice.ebai@student.howest.be>
2024-08-15 04:58:35 +0200

    Initial setup of Services class with dependency injection of MyDBContext.

--- CardVault/Services/Services.cs
+++ CardVault/Services/Services.cs
@@ -1,6 +1,15 @@
-﻿namespace CardVault.Services
+﻿using CardVault.Models;
+
+namespace CardVault.Services
 {
     public class Services
     {
+        private readonly MyDBContext dbContext;
+
+        public Services()
+        {
+            dbContext = new MyDBContext();
+        }
     }
+
 }
